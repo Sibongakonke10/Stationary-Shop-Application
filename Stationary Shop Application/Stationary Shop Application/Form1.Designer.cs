3777a19a78e309869ac4bd41b13eaa303ad1ed94
Sibongakonke10 <nqobanizibane2004@gmail.com>
2024-12-21 16:27:47 +0200

    Add files via upload
    Stationary Shop Application

---
+++ Stationary Shop Application/Stationary Shop Application/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace Stationary_Shop_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.noteBox = new System.Windows.Forms.TextBox();
+            this.booksBox = new System.Windows.Forms.TextBox();
+            this.pencilsBox = new System.Windows.Forms.TextBox();
+            this.pensBox = new System.Windows.Forms.TextBox();
+            this.rulersBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.amountBox = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.CloseBtn = new System.Windows.Forms.Button();
+            this.Clearbutton = new System.Windows.Forms.Button();
+            this.TotalBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.noteBox);
+            this.groupBox1.Controls.Add(this.booksBox);
+            this.groupBox1.Controls.Add(this.pencilsBox);
+            this.groupBox1.Controls.Add(this.pensBox);
+            this.groupBox1.Controls.Add(this.rulersBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(273, 278);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ITEMS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "NOTEPADS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "BOOKS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "PENCILS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "PENS";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "RULERS";
+            // 
+            // noteBox
+            // 
+            this.noteBox.Location = new System.Drawing.Point(145, 222);
+            this.noteBox.Name = "noteBox";
+            this.noteBox.Size = new System.Drawing.Size(100, 20);
+            this.noteBox.TabIndex = 4;
+            // 
+            // booksBox
+            // 
+            this.booksBox.Location = new System.Drawing.Point(145, 176);
+            this.booksBox.Name = "booksBox";
+            this.booksBox.Size = new System.Drawing.Size(100, 20);
+            this.booksBox.TabIndex = 3;
+            // 
+            // pencilsBox
+            // 
+            this.pencilsBox.Location = new System.Drawing.Point(145, 130);
+            this.pencilsBox.Name = "pencilsBox";
+            this.pencilsBox.Size = new System.Drawing.Size(100, 20);
+            this.pencilsBox.TabIndex = 2;
+            // 
+            // pensBox
+            // 
+            this.pensBox.Location = new System.Drawing.Point(145, 87);
+            this.pensBox.Name = "pensBox";
+            this.pensBox.Size = new System.Drawing.Size(100, 20);
+            this.pensBox.TabIndex = 1;
+            // 
+            // rulersBox
+            // 
+            this.rulersBox.Location = new System.Drawing.Point(145, 48);
+            this.rulersBox.Name = "rulersBox";
+            this.rulersBox.Size = new System.Drawing.Size(100, 20);
+            this.rulersBox.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.amountBox);
+            this.groupBox2.Location = new System.Drawing.Point(12, 296);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(273, 47);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Total Due";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Amount";
+            // 
+            // amountBox
+            // 
+            this.amountBox.Location = new System.Drawing.Point(145, 19);
+            this.amountBox.Name = "amountBox";
+            this.amountBox.ReadOnly = true;
+            this.amountBox.Size = new System.Drawing.Size(100, 20);
+            this.amountBox.TabIndex = 10;
+            this.amountBox.Text = "0";
+            this.amountBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.CloseBtn);
+            this.groupBox3.Controls.Add(this.Clearbutton);
+            this.groupBox3.Controls.Add(this.TotalBtn);
+            this.groupBox3.Location = new System.Drawing.Point(12, 350);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(511, 53);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.Location = new System.Drawing.Point(350, 20);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(148, 23);
+            this.CloseBtn.TabIndex = 2;
+            this.CloseBtn.Text = "CLOSE";
+            this.CloseBtn.UseVisualStyleBackColor = true;
+            this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // Clearbutton
+            // 
+            this.Clearbutton.Location = new System.Drawing.Point(186, 20);
+            this.Clearbutton.Name = "Clearbutton";
+            this.Clearbutton.Size = new System.Drawing.Size(148, 23);
+            this.Clearbutton.TabIndex = 1;
+            this.Clearbutton.Text = "CLEAR";
+            this.Clearbutton.UseVisualStyleBackColor = true;
+            this.Clearbutton.Click += new System.EventHandler(this.Clearbutton_Click);
+            // 
+            // TotalBtn
+            // 
+            this.TotalBtn.Location = new System.Drawing.Point(18, 20);
+            this.TotalBtn.Name = "TotalBtn";
+            this.TotalBtn.Size = new System.Drawing.Size(148, 23);
+            this.TotalBtn.TabIndex = 0;
+            this.TotalBtn.Text = "CALCULATE TOTAL";
+            this.TotalBtn.UseVisualStyleBackColor = true;
+            this.TotalBtn.Click += new System.EventHandler(this.TotalBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(412, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(282, 278);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "SCHOOLS STATIONARY SHOP";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox noteBox;
+        private System.Windows.Forms.TextBox booksBox;
+        private System.Windows.Forms.TextBox pencilsBox;
+        private System.Windows.Forms.TextBox pensBox;
+        private System.Windows.Forms.TextBox rulersBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox amountBox;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button CloseBtn;
+        private System.Windows.Forms.Button Clearbutton;
+        private System.Windows.Forms.Button TotalBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Stationary Shop Application/Stationary Shop Application/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stationary_Shop_Application
+{
+    public partial class Form1 : Form
+    {
+        decimal rulerCost=2;
+        decimal penCost=3.50m;
+        decimal pencilCost=2;
+        decimal bookCost=5;
+        decimal notepadCost=7.5m;
+        decimal totalAmt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TotalBtn_Click(object sender, EventArgs e)
+        {
+            if((rulersBox.Text==""))
+            {
+                MessageBox.Show("Rulers cannot be empty");
+            }
+            if ((pensBox.Text == ""))
+            {
+                MessageBox.Show("Pens cannot be empty");
+            }
+            if ((pencilsBox.Text == ""))
+            {
+                MessageBox.Show("Pencils cannot be empty");
+            }
+            if ((booksBox.Text == ""))
+            {
+                MessageBox.Show("Books cannot be empty");
+            }
+            if ((noteBox.Text == ""))
+            {
+                MessageBox.Show("Notepads cannot be empty");
+            }
+
+            else
+            {
+                try
+                {
+                    totalAmt = (rulerCost * decimal.Parse(rulersBox.Text)) + (penCost * decimal.Parse(pensBox.Text)) + (pencilCost * decimal.Parse(pencilsBox.Text)) + (bookCost * decimal.Parse(booksBox.Text)) + (notepadCost * decimal.Parse(noteBox.Text));
+                    amountBox.Text = totalAmt.ToString("C2");
+                }
+                catch
+                {
+                    MessageBox.Show("Please enter Valid Values");
+                }
+                
+            }
+        }
+
+        private void Clearbutton_Click(object sender, EventArgs e)
+        {
+            rulersBox.Text = " ";
+            pensBox.Text = " ";
+            pencilsBox.Text = " ";
+            booksBox.Text = " ";
+            noteBox.Text = " ";
+            amountBox.Text = " ";
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
